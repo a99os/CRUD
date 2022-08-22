b0e433cc2eebb1d8ad2755e289ac83db8601b0d8
a99os <abbosashirboev@gmail.com>
2022-08-22 12:27:19 +0500

    Strukturasi

---
+++ main.py
@@ -0,0 +1,28 @@
+import psycopg2
+
+class User():
+    def __init__(self) -> None:
+            
+    def connection(self):
+        pass
+    def register(self):
+        pass
+    def signIn(self):
+        pass
+    # login bormi yo'qmi?
+    def checkLogin(self):
+        pass
+    def changePassword(self):
+        self.name = "Kamoliddin"
+        pass
+    
+    @staticmethod
+    def generatePassword():
+        return "SASDA"
+
+    def getById(self):
+        pass
+    def deleteById(self):
+        pass
+    def checkLoginAndPassword(self):
+        pass
